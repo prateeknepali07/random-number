052e4b492eb301479deff9de4bd15477548447a5
AkShay Mahajan <vomaksh@gmail.com>
2019-01-05 18:33:50 +0530

    generate random no

--- src/App.js
+++ src/App.js
@@ -6,22 +6,43 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <h1>
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        </h1>
+        <Body />
       </div>
     );
+  }
+}
+
+class Body extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      no: 0,
+    };
+    this.getRandomNumber = this.getRandomNumber.bind(this);
+  }
+
+  getRandomNumber() {
+    //console.log('its work');
+    this.setState({ no: Math.floor(Math.random() * 100) });
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.getRandomNumber}>RandomNumber</button>
+        <Number mynumber={this.state.no} />
+      </div>
+    );
+  }
+}
+
+class Number extends Component {
+  render() {
+    return <div>{this.props.mynumber}</div>;
   }
 }
 
